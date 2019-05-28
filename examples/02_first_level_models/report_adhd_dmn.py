b85c1c3e06eaedea849ca0bcfff8ebd15cf3d50e
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2019-05-28 15:02:33 +0200

    Reports show rudimentary contrast info & design matrix; added reporter example

---
+++ examples/02_first_level_models/report_adhd_dmn.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+from nilearn import datasets
+from nilearn.input_data import NiftiSpheresMasker
+
+from nistats.first_level_model import FirstLevelModel
+from nistats.design_matrix import make_first_level_design_matrix
+
+from nistats.reporting.glm_reporter import generate_report
+
+
+def make_zmaps(first_level_model, contrasts):
+    """ Given a first model and contrasts, return the corresponding z-maps"""
+    z_maps = {}
+    for index, (contrast_id, contrast_val) in enumerate(contrasts.items()):
+        z_maps[contrast_id] = first_level_model.compute_contrast(
+                contrast_val, output_type='z_score')
+    return z_maps
+
+
+def create_report_adhd_dmn():
+    t_r = 2.
+    slice_time_ref = 0.
+    n_scans = 176
+    pcc_coords = (0, -53, 26)
+    adhd_dataset = datasets.fetch_adhd(n_subjects=1)
+    seed_masker = NiftiSpheresMasker([pcc_coords], radius=10, detrend=True,
+                                     standardize=True, low_pass=0.1,
+                                     high_pass=0.01, t_r=2.,
+                                     memory='nilearn_cache',
+                                     memory_level=1, verbose=0)
+    seed_time_series = seed_masker.fit_transform(adhd_dataset.func[0])
+    frametimes = np.linspace(0, (n_scans - 1) * t_r, n_scans)
+    design_matrix = make_first_level_design_matrix(frametimes, hrf_model='spm',
+                                                   add_regs=seed_time_series,
+                                                   add_reg_names=["pcc_seed"])
+    dmn_contrast = np.array([1] + [0] * (design_matrix.shape[1] - 1))
+    contrasts = {'seed_based_glm': dmn_contrast}
+    
+    first_level_model = FirstLevelModel(t_r=t_r, slice_time_ref=slice_time_ref)
+    first_level_model = first_level_model.fit(run_imgs=adhd_dataset.func[0],
+                                              design_matrices=design_matrix)
+    
+    output_filepath = 'generated_report.html'
+    z_maps = make_zmaps(first_level_model, contrasts)
+    generate_report(output_filepath, first_level_model,
+                                  contrasts=contrasts,
+                                  z_maps=z_maps,
+                                  mask=first_level_model.masker_.mask_img_,
+                                  design_matrices=first_level_model.design_matrices_,
+                                  anat=datasets.load_mni152_template(),
+                                  )
+    # generate_subject_stats_report(output_filepath,
+    #                               contrasts=contrasts,
+    #                               z_maps=z_maps,
+    #                               mask=first_level_model.masker_.mask_img_,
+    #                               design_matrices=first_level_model.design_matrices_,
+    #                               anat=datasets.load_mni152_template(),
+    #                               )
+
+
+if __name__ == '__main__':
+    create_report_adhd_dmn()
+
--- nistats/reporting/glm_reporter.py
+++ nistats/reporting/glm_reporter.py
@@ -1,26 +1,75 @@
 import string
+import os
+import pprint
 
-# template_text = (
-#     '<!DOCTYPE html>\n'
-#     '<html lang="en">\n'
-#     '<head>\n'
-#     '<meta charset="UTF-8">\n'
-#     '<title>Title</title>\n'
-#     '</head>\n'
-#     '<body>\n'
-#
-# </body>
-# </html>
-# '
-# )
-with open('report_template.html') as html_file_obj:
-    html_template_text = html_file_obj.read()
+import numpy as np
+
+from nistats.design_matrix import check_design_matrix
+
+
+html_template_path = os.path.join(os.path.dirname(__file__), 'report_template.html')
+def generate_report(output_path, model, **kwargs):
+    with open(html_template_path) as html_file_obj:
+        html_template_text = html_file_obj.read()
+        
+    # print(html_template_text)
+    report_template = string.Template(html_template_text)
+    contrasts = pretty_print_mapping_of_sequence(kwargs['contrasts'])
+    print(repr(contrasts))
+    design_matrix_plot = save_design_matrix_plot(model)
+    report_values = {'Title': 'Test Report',
+                     'contrasts': contrasts,
+                     'design_matrix_binary': design_matrix_plot,
+                     }
+    report_text = report_template.safe_substitute(**report_values)
     
-# print(html_template_text)
-report_template = string.Template(html_template_text)
+    print(report_text)
+    with open(output_path, 'w') as html_write_obj:
+        html_write_obj.write(report_text)
 
-report_values = {'Title': 'Test Report'}
-report_text = report_template.safe_substitute(**report_values)
-print(report_text)
-with open('generated_report.html', 'w') as html_write_obj:
-    html_write_obj.write(report_text)
+
+def save_design_matrix_plot(model):
+    from matplotlib import pyplot as plt
+    
+    design_matrix = model.design_matrices_[0]
+    fig = plt.figure()  #TODO: code from _plot_matrices.plot_design_matrix(). Refactor target.
+    ax = fig.add_subplot(1, 1, 1)
+    _, X, names = check_design_matrix(design_matrix)
+    X = X / np.maximum(1.e-12, np.sqrt(
+            np.sum(X ** 2, 0)))  # pylint: disable=no-member
+
+    ax.imshow(X, interpolation='nearest', aspect='auto')
+    ax.set_label('conditions')
+    ax.set_ylabel('scan number')
+
+    ax.set_xticks(range(len(names)))
+    ax.set_xticklabels(names, rotation=60, ha='right')
+
+    plt.tight_layout()
+    
+    # dmtx_fpath = os.path.abspath('./results/dmtx.png')
+    dmtx_fpath = 'dmtx.png'
+    fig.savefig(dmtx_fpath)
+    return dmtx_fpath
+    
+    # experimenting with embedding the image binary into the html
+    # fig.canvas.draw()
+    # buf = fig.canvas.tostring_rgb()
+    # import base64
+    # buf64 = base64.b64encode(buf)
+    # design_matrix_image_html_insert = 'data:image/png;base64,{}'.format((buf64))
+    # design_matrix_image_html_insert = '<img src="data:image/png;base64,{}">'.format(buf64.rstrip('\r'))
+    # return design_matrix_image_html_insert
+    
+    
+def pretty_print_mapping_of_sequence(any_dict):
+    output_text = []
+    for key, value in any_dict.items():
+        formatted_value = pprint.pformat(value)
+        line_text = '{} : {}'.format(key, formatted_value)
+        output_text.append(line_text)
+    return '\n'.join(output_text)
+        
+    
+if __name__ == '__main__':
+    generate_report('generated_report.html', None)
