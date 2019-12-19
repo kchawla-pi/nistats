a86491907cacd229465b6f5aa569088ceef493d5
James Kent <james-kent@uiowa.edu>
2019-12-19 11:51:39 -0600

    fix code formatting and do not standardize

--- examples/02_first_level_models/plot_predictions_residuals.py
+++ examples/02_first_level_models/plot_predictions_residuals.py
@@ -1,7 +1,7 @@
 """
 Predicted time series and residuals
 ===================================
- 
+
 Here we fit a First Level GLM with the `minimize_memory`-argument set to `False`.
 By doing so, the `FirstLevelModel`-object stores the residuals, which we can then inspect.
 Also, the predicted time series can be extracted, which are useful to assess the quality of the model fit.
@@ -12,7 +12,6 @@ Also, the predicted time series can be extracted, which are useful to assess the
 # Import modules
 # --------------
 from nistats.datasets import fetch_spm_auditory
-from nilearn.plotting import plot_stat_map
 from nilearn import input_data, image
 import matplotlib.pyplot as plt
 from nilearn import plotting, masking
@@ -28,8 +27,8 @@ fmri_img = image.concat_imgs(subject_data.func)
 mean_img = image.mean_img(fmri_img)
 mask = masking.compute_epi_mask(mean_img)
 
-# Clean (demean) and smooth data
-fmri_img = image.clean_img(fmri_img)
+# Clean and smooth data
+fmri_img = image.clean_img(fmri_img, standardize=False)
 fmri_img = image.smooth_img(fmri_img, 5.)
 
 # load events
@@ -65,10 +64,10 @@ plotting.plot_stat_map(z_map, bg_img=mean_img, threshold=3.1)
 # ----------------------------
 
 table = get_clusters_table(z_map, stat_threshold=3.1,
-        cluster_threshold=20).set_index('Cluster ID', drop=True)
+                           cluster_threshold=20).set_index('Cluster ID', drop=True)
 table.head()
 
-masker = input_data.NiftiSpheresMasker(table.loc[range(1,7), ['X', 'Y', 'Z']].values)
+masker = input_data.NiftiSpheresMasker(table.loc[range(1, 7), ['X', 'Y', 'Z']].values)
 
 real_timeseries = masker.fit_transform(fmri_img)
 predicted_timeseries = masker.fit_transform(fmri_glm.predicted)
@@ -78,13 +77,13 @@ predicted_timeseries = masker.fit_transform(fmri_glm.predicted)
 # Plot predicted and actual time series for 6 most significant clusters
 # ---------------------------------------------------------------------
 for i in range(1, 7):
-    plt.subplot(2,  3, i)
+    plt.subplot(2, 3, i)
     plt.title('Cluster peak {}\n'.format(table.loc[i, ['X', 'Y', 'Z']].tolist()))
     plt.plot(real_timeseries[:, i-1], c='k', lw=2)
     plt.plot(predicted_timeseries[:, i-1], c='r',  ls='--', lw=2)
     plt.xlabel('Time')
     plt.ylabel('Signal intensity')
-    
+
 plt.gcf().set_size_inches(12, 7)
 plt.tight_layout()
 
@@ -101,7 +100,7 @@ resid = masker.fit_transform(fmri_glm.residuals)
 
 
 for i in range(1, 7):
-    plt.subplot(2,  3, i)
+    plt.subplot(2, 3, i)
     plt.title('Cluster peak {}\n'.format(table.loc[i, ['X', 'Y', 'Z']].tolist()))
     plt.hist(resid[:, i-1])
     print('Mean residuals: {}'.format(resid[:, i-1].mean()))
