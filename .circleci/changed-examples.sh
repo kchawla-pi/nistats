0dd48f7b7dbcf5bfd0ca0f31f45016029a85fe6c
Kshitij Chawla (kchawla-pi) <kc.insight.pi@gmail.com>
2020-01-30 23:28:24 +0100

    Added code to build specific source files as well

--- .circleci/changed-examples.sh
+++ .circleci/changed-examples.sh
@@ -1,1 +1,12 @@
-git diff --name-only master | grep -e examples/ | grep -e /**.py
+changed_examples=`git diff --name-only master | grep -e examples/ | grep -e /**/*.py`
+changed_sources=` git diff --name-only master | grep -v examples/ | grep -e /**/*.py`
+
+for changed_example_ in $changed_examples
+do
+  python -m sphinx -D sphinx_gallery_conf.filename_pattern=$changed_example_ -b html -d _build/doctrees . _build/html
+done
+
+for changed_source_ in $changed_sources
+do
+  python -m sphinx -D sphinx_gallery_conf.filename_pattern=$changed_source_ -b html -d _build/doctrees . _build/html
+done
